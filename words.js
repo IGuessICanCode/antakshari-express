38530504ef5f642d32b130ffa46b312d422aee14
IGuessICanCode <tainwala.arushi@gmail.com>
2026-06-17 15:06:05 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,19 @@
+export const WORD_LISTS = {
+  bollywood: [
+    "raat","sapna","dil","ishq","yaadein","khushi","pyaar","hum","tum","din",
+    "gham","zindagi","jaan","bewafa","safar","saath","raasta","hawaa","taare",
+    "ghar","sheher","chaand","pal","rang","junoon","baatein","aankh","mere",
+    "tere","door","paas","pani","kuch","duniya","piya","phir","udd","pankh",
+    "allah","main","bina","jawaani","chori","fevicol","sunita","nach","laila",
+    "saturday","sajna","nahi","rani","raja","sauda","lift","kyaa","kyun","kala"
+  ],
+
+  christmas: [
+    "snow","bell","star","night","joy","peace","angel","winter","gift","tree",
+    "santa","reindeer","sleigh","carol","holy","silent","merry","bright","light",
+    "chimney","stocking","candle","frost","cold","cheer","family","home","love",
+    "wish","faith","snowman","icicle","glory","heaven","king","child","shepherd",
+    "present","december","noel","hallelujah","midnight","season","celebrate",
+    "magic","spirit","wonder","hope","blessing","evergreen"
+  ]
+};
